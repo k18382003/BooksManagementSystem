57929d964235c3ce4c02bfdcdbbc0769a292c3b8
summer <k18382003@gmail.com>
2021-11-09 23:12:07 +0800

    加入專案檔案。

---
+++ Books Management System/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace Books_Management_System
+{
+    partial class frmAuthor
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAuthorID = new System.Windows.Forms.Label();
+            this.lblAuthorName = new System.Windows.Forms.Label();
+            this.lblYearBorn = new System.Windows.Forms.Label();
+            this.txtAuthorID = new System.Windows.Forms.TextBox();
+            this.txtAutorName = new System.Windows.Forms.TextBox();
+            this.txtYearBorn = new System.Windows.Forms.TextBox();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAuthorID
+            // 
+            this.lblAuthorID.AutoSize = true;
+            this.lblAuthorID.Location = new System.Drawing.Point(37, 26);
+            this.lblAuthorID.Name = "lblAuthorID";
+            this.lblAuthorID.Size = new System.Drawing.Size(48, 16);
+            this.lblAuthorID.TabIndex = 0;
+            this.lblAuthorID.Text = "Author ID";
+            // 
+            // lblAuthorName
+            // 
+            this.lblAuthorName.AutoSize = true;
+            this.lblAuthorName.Location = new System.Drawing.Point(37, 67);
+            this.lblAuthorName.Name = "lblAuthorName";
+            this.lblAuthorName.Size = new System.Drawing.Size(64, 16);
+            this.lblAuthorName.TabIndex = 1;
+            this.lblAuthorName.Text = "Author Name";
+            // 
+            // lblYearBorn
+            // 
+            this.lblYearBorn.AutoSize = true;
+            this.lblYearBorn.Location = new System.Drawing.Point(37, 105);
+            this.lblYearBorn.Name = "lblYearBorn";
+            this.lblYearBorn.Size = new System.Drawing.Size(51, 16);
+            this.lblYearBorn.TabIndex = 2;
+            this.lblYearBorn.Text = "Year Born";
+            // 
+            // txtAuthorID
+            // 
+            this.txtAuthorID.Location = new System.Drawing.Point(129, 23);
+            this.txtAuthorID.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtAuthorID.Name = "txtAuthorID";
+            this.txtAuthorID.Size = new System.Drawing.Size(173, 21);
+            this.txtAuthorID.TabIndex = 3;
+            // 
+            // txtAutorName
+            // 
+            this.txtAutorName.Location = new System.Drawing.Point(129, 64);
+            this.txtAutorName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtAutorName.Name = "txtAutorName";
+            this.txtAutorName.Size = new System.Drawing.Size(359, 21);
+            this.txtAutorName.TabIndex = 4;
+            // 
+            // txtYearBorn
+            // 
+            this.txtYearBorn.Location = new System.Drawing.Point(129, 102);
+            this.txtYearBorn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtYearBorn.Name = "txtYearBorn";
+            this.txtYearBorn.Size = new System.Drawing.Size(173, 21);
+            this.txtYearBorn.TabIndex = 5;
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(153, 145);
+            this.btnPrevious.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(67, 35);
+            this.btnPrevious.TabIndex = 6;
+            this.btnPrevious.Text = "<<Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(286, 145);
+            this.btnNext.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(67, 35);
+            this.btnNext.TabIndex = 7;
+            this.btnNext.Text = "Next>>";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(96, 201);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(63, 35);
+            this.btnEdit.TabIndex = 8;
+            this.btnEdit.Text = "&Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(225, 201);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(63, 35);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "&Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(347, 201);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(63, 35);
+            this.btnCancel.TabIndex = 10;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(96, 244);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(63, 32);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "&Add New";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(225, 244);
+            this.btnDel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(63, 32);
+            this.btnDel.TabIndex = 12;
+            this.btnDel.Text = "&Delete";
+            this.btnDel.UseVisualStyleBackColor = true;
+            // 
+            // btnDone
+            // 
+            this.btnDone.Location = new System.Drawing.Point(347, 244);
+            this.btnDone.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(63, 32);
+            this.btnDone.TabIndex = 13;
+            this.btnDone.Text = "Do&ne";
+            this.btnDone.UseVisualStyleBackColor = true;
+            // 
+            // frmAuthor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 289);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.txtYearBorn);
+            this.Controls.Add(this.txtAutorName);
+            this.Controls.Add(this.txtAuthorID);
+            this.Controls.Add(this.lblYearBorn);
+            this.Controls.Add(this.lblAuthorName);
+            this.Controls.Add(this.lblAuthorID);
+            this.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmAuthor";
+            this.Text = "Authors";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmClosing);
+            this.Load += new System.EventHandler(this.frmAuthor_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAuthorID;
+        private System.Windows.Forms.Label lblAuthorName;
+        private System.Windows.Forms.Label lblYearBorn;
+        private System.Windows.Forms.TextBox txtAuthorID;
+        private System.Windows.Forms.TextBox txtAutorName;
+        private System.Windows.Forms.TextBox txtYearBorn;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnDone;
+    }
+}
+
---
+++ Books Management System/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Books_Management_System
+{
+    public partial class frmAuthor : Form
+    {
+        public frmAuthor()
+        {
+            InitializeComponent();
+        }
+
+        MySqlConnection mySqlConnection;
+        MySqlCommand cmd;
+        MySqlDataAdapter daAuthors;
+        DataTable dtAuthors;
+        CurrencyManager authorsManager;
+        
+        private void frmAuthor_Load(object sender, EventArgs e)
+        {
+            string cnnString = "server=127.0.0.1;uid=sa;pwd=P@ssword;database=mydatabase";
+
+            try
+            {
+                mySqlConnection = new MySqlConnection();
+                mySqlConnection.ConnectionString = cnnString;
+                mySqlConnection.Open();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            try
+            {
+                cmd = new MySqlCommand("SELECT * FROM Authors Order by Author", mySqlConnection);
+                daAuthors = new MySqlDataAdapter();
+                dtAuthors = new DataTable();
+                daAuthors.SelectCommand = cmd;
+                daAuthors.Fill(dtAuthors);
+
+                txtAuthorID.DataBindings.Add("Text", dtAuthors, "Au_ID");
+                txtAutorName.DataBindings.Add("Text", dtAuthors, "Author");
+                txtYearBorn.DataBindings.Add("Text", dtAuthors, "Year_Born");
+                authorsManager =(CurrencyManager)BindingContext[dtAuthors];
+                authorsManager.Position = 0;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            authorsManager.Position--;
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            authorsManager.Position++;
+        }
+
+        private void frmClosing(object sender, FormClosingEventArgs e)
+        {
+            mySqlConnection.Close();
+            mySqlConnection.Dispose();
+            cmd.Dispose();
+            daAuthors.Dispose();
+            dtAuthors.Dispose();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Record saved", "Save", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
